49fd73a5a1e9fcc824ba0fa23e9ac827b9953871
Alex Ho <alextho4@hotmail.com>
2016-02-13 21:03:53 -0600

    Unity Game
    added assets, starting scripting

---
+++ Rhythm Factory/Assets/Scripts/noteAccept.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class noteAccept : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+
+        }
+    }
+}
---
+++ Rhythm Factory/Assets/Scripts/noteScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Projectile behavior
+/// </summary>
+public class noteScore : MonoBehaviour
+{
+    void Start()
+    {
+        // 2 - Limited time to live to avoid any leak
+        Destroy(gameObject, 1); // 1sec
+    }
+}
